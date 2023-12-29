c620774e09b592a56917b6b8f75aff7dd0af2a93
Alcadeius <andresentanu3@gmail.com>
2023-12-29 21:21:11 +0800

    base fondation

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function luas() {
+  document.getElementById("luas").style.display = "inline-block";
+  document.getElementById("kll").style.display = "none";
+}
+function keliling() {
+  document.getElementById("kll").style.display = "inline-block";
+  document.getElementById("luas").style.display = "none";
+}
